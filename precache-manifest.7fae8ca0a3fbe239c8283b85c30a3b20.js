eb94ceca087ed2df5cb61aba68bfc40535e68af6
ivanavocode <ivanavocode@gmail.com>
2019-09-03 14:42:27 +0600

    update project

---
+++ precache-manifest.7fae8ca0a3fbe239c8283b85c30a3b20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ae63f62ca7503b2377a3ba744945cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23d271af9b11c059976c",
+    "url": "/static/css/main.30bf1f4f.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "23d271af9b11c059976c",
+    "url": "/static/js/main.18fd7aae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
